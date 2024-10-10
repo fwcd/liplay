4c019845e319101c84da4c1ecd247ba0711568a1
fwcd <fwcd@fwcd.dev>
2024-10-10 18:54:54 +0200

    Set up basic lighthouse app

---
+++ README.md
@@ -0,0 +1,6 @@
+# Liplay
+
+[![crates.io](https://img.shields.io/crates/v/liplay)](https://crates.io/crates/liplay)
+[![Build](https://github.com/fwcd/liplay/actions/workflows/build.yml/badge.svg)](https://github.com/fwcd/liplay/actions/workflows/build.yml)
+
+Video player for Project Lighthouse.
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use clap::Parser;
+use lighthouse_client::{protocol::{Authentication, Frame}, Lighthouse, TokioWebSocket, LIGHTHOUSE_URL};
+use tracing::info;
+use tokio::time;
+use std::time::Duration;
+
+async fn run(mut lh: Lighthouse<TokioWebSocket>) -> Result<()> {
+    info!("Connected to the Lighthouse server");
+
+    loop {
+        lh.put_model(Frame::fill(rand::random())).await?;
+        info!("Sent frame");
+
+        time::sleep(Duration::from_secs(1)).await;
+    }
+}
+
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    /// The username.
+    #[arg(short, long, env = "LIGHTHOUSE_USER")]
+    username: String,
+    /// The API token.
+    #[arg(short, long, env = "LIGHTHOUSE_TOKEN")]
+    token: String,
+    /// The server URL.
+    #[arg(long, env = "LIGHTHOUSE_URL", default_value = LIGHTHOUSE_URL)]
+    url: String,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt().init();
+    _ = dotenvy::dotenv();
+
+    let args = Args::parse();
+    let auth = Authentication::new(&args.username, &args.token);
+    let lh = Lighthouse::connect_with_tokio_to(&args.url, auth).await?;
+
+    run(lh).await
+}
